63971c4d3abb0cbe1ab82b2856974859f839a7d3
dmoussalli <35779765+dmoussalli@users.noreply.github.com>
2019-04-03 12:03:06 -0400

    Update test_qsolve.R

--- test_qsolve.R
+++ test_qsolve.R
@@ -1,58 +1,47 @@
-#install.packages("testthat")
-require(testthat) #loading the library testtaht
-source("~/desktop/qsolve.R") #load the .r file to be tested
 
-#test if there is two distinct roots
-test_that("Distinct roots",{
-  roots <- qsolve(1,7,2)
-  
-  expect_that( roots, is_a("numeric") )
-  expect_that( length(roots), equals(2) )
-  expect_that( roots[1] <= roots[2], is_true() )
-  
+# Reference 1
+#   Title: Example of unit testing R code with testthat
+#   Published on: June 12, 2013
+#   Author: John
+#   Accessibility: https://www.johndcook.com/blog/2013/06/12/example-of-unit-testing-r-code-with-testthat/
+
+# to run this test: < test_file("C:/Users/User/Desktop/School/2610 - R Prog for Data Science/qsolve.R") >
+
+library(testthat) #loading the library testtaht
+
+test_that("Distinct roots", {
+    roots <- qsolve(1, 7, 12)
+    expect_that( roots, is_a("numeric") )
+    expect_that( length(roots), equals(2) )
+    expect_that( roots[1] < roots[2], is_true() )
 })
 
-test_that("only one root", {
-  
-  roots <- qsolve(1, 6000, 9000000)
-  
-  expect_that( length(roots), equals(1) )
-  
-  expect_that( roots, equals(-3000) )
-  
-  # Test whether ABSOLUTE error is within 0.1 
-  expect_that( roots, equals(-3000.01, tolerance  = 0.1) )
-  
-  # Test whether RELATIVE error is within 0.1
-  # To test relative error, set 'scale' equal to expected value.
-  # See base R function all.equal for optional argument documentation.
-  expect_equal( roots, -3001, tolerance  = 0.1, scale=-3001) 
+test_that("Repeated root", {
+    roots <- qsolve(1, 6000, 9000000)
+    expect_that( length(roots), equals(2) )
+    expect_that( roots, equals(c(-3000, -3000)) )
+    expect_that( roots, equals(c(-3000.01, -3000.01), tolerance  = 0.1) )# Test whether ABSOLUTE error is within 0.1 
+    
+    expect_equal( roots, c(-3001, -3001), tolerance  = 0.1, scale=-3001) 
 })
 
-#test if the disc (b^2-4ac) is less than 0 which will return null from qsolve.R
-
-test_that("disc is less than 0", {
-  roots <- qsolve(1,4,1)
-  expect_that(roots, is_null())
+test_that("A, B, and C should be finite real numbers", {
+    expect_that( qsolve("a", 2, 1), equals(NA) )    # Test for finite error 
+    expect_that( qsolve(1, "2", 1), equals(NA) )    # Test for finite error  
+    expect_that( qsolve(1, NA, 2), equals(NA) )     # Test for finite error 
+    expect_that( qsolve(1, 10e308, 1), equals(NA) ) # Test for finite error
+    
+    ox <- qsolve("fred", 2, 1)
+    expect_true(is.na(ox) && length(ox)==1)
 })
 
-
-#test
-test_that("a is zero",{
-  expect_that( qsolve(1, 2, 3), throws_error("zero") )
-  
+test_that("B^2 must be smaller than Inf", {
+    expect_that( qsolve(1, 1e308, 1), equals(NA) )  # Test for Inf error  
+    expect_that( qsolve(5, 1e210, 5), equals(NA) )  # Test for Inf error  
 })
 
-
-test_that("Polynomial must be quadratic", {
-  
-  # Test for ANY error                     
-  expect_that( qsolve(1, 2, 3), throws_error() )
-  
-  # Test specifically for an error string containing "zero"
-  expect_that( qsolve(0, 2, 3), throws_error("zero") )
-  
-  # Test specifically for an error string containing "zero" or "Zero" using regular expression
-  expect_that( qsolve(0, 2, 3), throws_error("[zZ]ero") )
+test_that("A cannot be zero", {
+    expect_that( qsolve(0, 2, 3), equals(NA) )# Test for ANY error  
+    expect_that( qsolve(0, 4, 3), equals(NA) )# Test specifically for an error string containing "zero"
+    expect_that( qsolve(0, 12, 4), equals(NA) )# Test specifically for an error string containing "zero" or "Zero" using regular expression
 })
-
